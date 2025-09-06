5916ef2b57eb09ed595f6f09c9e3f32cfdaa7240
Leandro Ferreira <leandro.ferreira@gmail.com>
2025-09-05 23:09:24 -0400

    removing log4j xml

--- MA/buildimage/build.sh
+++ MA/buildimage/build.sh
@@ -17,20 +17,20 @@ case "$2" in
         docker buildx build --platform linux/amd64,linux/arm64 -t leandrovo/ma-plugins-$2:$1 -f DockerfileRedis .
         ;;
     kafka)
-        echo "Selected domain: kafka"
+        echo "Selected : kafka"
         ;;
     zoo)
-        echo "Selected domain: zoo"
+        echo "Selected : zoo"
         ;;
     mongo)
-        echo "Selected domain: mongo"
+        echo "Selected : mongo"
         ;;
     all)
-        echo "Selected domain: all"
+        echo "Selected : all"
         docker buildx build --platform linux/amd64,linux/arm64 -t leandrovo/ma-plugins-$2:$1 -f Dockerfile .
         ;;
     *)
-        echo "invalid domain: $2, must be redis, kafka, zoo, mongo, or all"
+        echo "invalid : $2, must be redis, kafka, zoo, mongo, or all"
         exit 1
         ;;
 esac
